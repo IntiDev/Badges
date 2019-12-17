136bd2c234567e44c898661f4966a2c616cd0f3f
Inti <yeseniase@outlook.it>
2019-12-17 10:36:31 -0600

    ADD: elementos del componente BADGE

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import brandLogo from '../images/logo_design_bjnx.svg';
+import avatar from '../images/female_avatar_l3ey.svg'
 
 class Badge extends React.Component {
   render() {
-    return  <h1> Aquí codeando un rato </h1>
+    return <div>
+      <div>
+        <img src={ brandLogo } alt="Logo de la marca"/>
+      </div>
+      
+      <div>
+        <img src={ avatar} alt="Avatar"/>
+        <h1> Nombre <br/> de la persona</h1>
+      </div>
+
+      <div>
+        <p> Frontend developer </p>
+        <p> @IntiDev</p>
+      </div>
+
+      <div>
+        <p> #Coding </p>
+      </div>
+    </div>
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const jsx = <h1>Hello, Platzi Badges :D!</h1>;
-// const element = React.createElement(
-//   'a',
-//   {href: 'https://platzi.com'},
-//   'Ir a platzi'
-//   );
-const name = "Inti ;)";
-const element = React.createElement("h1", {}, `Hola soy ${name}`);
-const sum = () => 3+3;
-//{__expresiones__} que se puedan EVALUAR -> Los valores falsos NO se muestran
-const elementJSX = <h1> Hola soy {name}</h1> // variable / expresión {2 + 2 } / llamada a función {sum()}
+import Badge from './components/Badge'
+
 const container = document.getElementById("app");
 
-const contenedorJSX = (
-  <div>
-    <h1> Hola :D</h1>
-    <p> Algo de texto para mostrar xP</p>
-  </div>
-);
-
-// ReactDOM.render(__qué__, __dónde__);
-// ReactDOM.render(element, container);
-// ReactDOM.render(elementJSX, container);
-ReactDOM.render(contenedorJSX, container);
+ReactDOM.render(<Badge/>, container);
