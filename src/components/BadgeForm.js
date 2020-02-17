5e86562293f76ade58e11e6169d91b076b754c02
IntiDev <yeseniase@outlook.it>
2020-02-17 06:28:38 -0600

    Se enlazan eventos del formulario

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,20 +1,42 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
-    render() {
-        return (
-           <>
-            <h1> New attendant </h1>
-            <form>
-                <div className='form-group'>
-                    <label> First Name </label>
-                    <input onChange={this.handleChange} className='form-control' type='text' name='firstName'/>
-                </div>
-                <button className='btn btn-primary'> Save </button>
-            </form>
-           </>
-        )
-    }
+  handleChange = e => {
+    console.log({
+        name: e.target.name,
+        value: e.target.value,
+    });
+  };
+
+  handleClick = e => {
+    console.log('Button was clicked');
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('Form was submited');
+  };
+
+  render() {
+    return (
+      <>
+        <h1> New attendant </h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <div className='form-group'>
+            <label> First Name </label>
+            <input
+              onChange={this.handleChange}
+              className='form-control'
+              type='text'
+              name='firstName'
+            />
+          </div>
+          <button onClick={this.handleClick} className='btn btn-primary'> Save </button>
+        </form>
+      </>
+    );
+  }
 }
 
 export default BadgeForm;
