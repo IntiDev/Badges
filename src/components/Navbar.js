28d2e6e280662d9c4ae28d00095516799cb17ece
IntiDev <yeseniase@outlook.it>
2020-02-18 23:58:28 -0600

    Prueba de link para navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles/Navbar.css';
 import brandLogo from '../images/badge-header.svg';
+import { BrowserRouter } from 'react-router-dom';
 
 class Navbar extends React.Component {
   render() {
     return (
       <div className='Navbar'>
         <div className='container-fluid'>
+        <BrowserRouter>
           <Link className='Navbar__brand' to="/">
             <img className='Navbar__brand-logo' src={brandLogo} alt='alt'/>
             <span className="font-weight-light"> Platzi </span>
             <span className="font-weight-bold"> Conf </span>
           </Link>
+          </BrowserRouter>
         </div>
       </div>
     )
